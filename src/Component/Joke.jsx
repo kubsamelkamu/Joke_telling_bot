7e9c3ee2861cee7d7b5def2ceed1f9facd30b797
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-02 10:46:44 +0300

    component to be rendered

--- src/Component/Joke.jsx
+++ src/Component/Joke.jsx
@@ -3,5 +3,13 @@ import { useState } from "react";
 function JokeDisplay(){
     const[joke,setJoke] = useState('');
     const[isSpeaking,setIsSpeaking] = useState(false);
-    
+
+    return(
+        <div>
+            <p>Joke displayed here.</p>
+            <button>Another Joke</button>
+            <button>Speak Aloud</button>
+        </div>
+    );
+
 }
