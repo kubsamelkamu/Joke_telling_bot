c5cf2431d3c0acd3ccb373ec929c6f6b6caac690
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-08-02 10:57:52 +0300

    import JokeDisplay

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import JokeDisplay from './Component/Joke'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <JokeDisplay/>
   </React.StrictMode>,
 )
